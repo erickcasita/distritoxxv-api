fd07134eb4f1de70acfd4907e93472d1c6254c05
Yosimar Zahid Aquino Sosa <yosimaraquinos@proton.me>
2024-02-21 17:11:27 -0600

    Se agrego el endpoint getByName para towns

--- src/controllers/town.controllers.js
+++ src/controllers/town.controllers.js
@@ -13,4 +13,18 @@ const getAll = async (req, res) => {
     }
 }
 
-export { getAll }
+const getByName = async (req, res) => {
+    try {
+        const { name } = req.params;
+        const query = await Town.findOne({ name: name })
+        if (query) {
+            return query
+                ? res.status(200).json({ status: 200, response: "success", data: query })
+                : res.status(404).json({ status: 404, responseError: "not found" })
+        }
+    } catch (error) {
+        return res.status(500).json({ status: 500, responseError: error })
+    }
+}
+
+export { getAll, getByName }
